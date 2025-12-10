9083e512baaa0a90a9bc06e402d7e6616f8f9983
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 01:15:53 +0100

    Apply swift-format and lint fixes

--- Package.swift
+++ Package.swift
@@ -13,10 +13,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-rfc-6570",
     platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11)
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26)
     ],
     products: [
         .library(name: .rfc6570, targets: [.rfc6570]),
